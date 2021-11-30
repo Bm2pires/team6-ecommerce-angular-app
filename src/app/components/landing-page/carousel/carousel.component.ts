a7d0c2e1f2764bf04f5678183e849555291f69e5
avahora866 <avahora1@hotmail.com>
2021-11-30 13:08:26 +0000

    Finishde off Carousel

--- src/app/components/landing-page/carousel/carousel.component.ts
+++ src/app/components/landing-page/carousel/carousel.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel.component.html', styleUrls: ['./carousel.component.css']})
 export class NgbdCarouselBasic {
-  url1 = "https://picsum.photos/1400/500?random=1"
-  url2 = "https://picsum.photos/1400/500?random=2"
-  url3 = "https://picsum.photos/1400/500?random=3"
 
-  images = [this.url1, this.url2, this.url3];
-
+  images = ["../../../../assets/images/placeholderOne.jpg", "../../../../assets/images/placeholderTwo.jpg", "../../../../assets/images/placeholderThree.jpg"];
 }
