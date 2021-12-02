b5f4ba2ff91daa640d6db6280edaffe4aef287a5
avahora866 <avahora1@hotmail.com>
2021-12-02 11:03:56 +0000

    Navabar basic setup compleated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NgbdCarouselBasic } from './components/landing-page/carousel/carousel.component';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbDropdown, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NgbdCarouselBasic
+    NgbdCarouselBasic,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbCarouselModule
+    NgbCarouselModule,
+    NgbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
