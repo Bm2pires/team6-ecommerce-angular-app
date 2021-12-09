4c64e73fafc8f1b2a77495eae27f894f829d8862
Bredan Pires <piresbredan@gmail.com>
2021-12-09 17:29:28 +0000

    added some css styling and some form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  onSubmit() {
+  onSubmit(form: NgForm) {
     // code to execute after form is submitted
+    console.log('Submitted');
+    console.log(form);
+
+    this.email = form.value.email;
+    this.password = form.value.password;
+    console.log(this.email);
+    console.log(this.password);
   }
 }
