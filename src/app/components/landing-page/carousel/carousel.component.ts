d5969511a6a6c67a02ee2394961b613666d20c25
avahora866 <avahora1@hotmail.com>
2021-12-15 14:30:48 +0000

    minor changes

--- src/app/components/landing-page/carousel/carousel.component.ts
+++ src/app/components/landing-page/carousel/carousel.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
-@Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel.component.html', styleUrls: ['./carousel.component.css']})
+@Component({selector: 'ngbd-carousel-basic',
+            templateUrl: './carousel.component.html',
+            styleUrls: ['./carousel.component.css']})
+
 export class NgbdCarouselBasic {
 
   images = ["../../../../assets/images/placeholderOne.jpg", "../../../../assets/images/placeholderTwo.jpg", "../../../../assets/images/placeholderThree.jpg"];
+
 }
