bb3dac20b02841639aa003f4fa75a49aa98323a8
Bredan Pires <piresbredan@gmail.com>
2021-12-15 15:22:49 +0000

    added login component under components directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NgbdCarouselBasic } from './components/landing-page/carousel/carousel.component';
-import { NgbCarouselModule, NgbDropdown, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbCarouselModule,
+  NgbDropdown,
+  NgbDropdownModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-
 
 @NgModule({
   declarations: [
@@ -17,14 +20,9 @@ import { FooterComponent } from './components/footer/footer.component';
     NgbdCarouselBasic,
     NavbarComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    NgbCarouselModule,
-    NgbDropdownModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, NgbCarouselModule, NgbDropdownModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
