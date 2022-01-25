e69fa68fc19764dc674452a8b5ec41c83d24a7e3
avahora866 <avahora1@hotmail.com>
2022-01-25 16:40:05 +0000

    Added user and repository entities

---
+++ ecommerce-backend/ecommerce-backend/src/main/java/com/team6/ecommercebackend/repositries/UserRepository.java
@@ -0,0 +1,9 @@
+package com.team6.ecommercebackend.repositries;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.team6.ecommercebackend.entities.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
