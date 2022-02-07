0dcf0b8bb5409f7d66df31c26cf88840d048ee06
Saurabh Kene <saurabh97kene@gmail.com>
2022-02-08 01:11:44 +0530

    register user

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from './loginUser';
+import { User } from './registerUser';
 import { Observable } from 'rxjs';
 
 @Injectable({
